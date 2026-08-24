bbb0abadeb14f2b71895f2c48691497ed98d5a76
syslikmachete <opflexxy@gmail.com>
2026-08-24 20:29:02 +0400

    feat: add burger menu script

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,12 @@
-/* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.header__menu');
+const navToggle = document.querySelector('.header__button');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('header__menu--closed')) {
+    navMain.classList.remove('header__menu--closed');
+    navMain.classList.add('header__menu--opened');
+  } else {
+    navMain.classList.add('header__menu--closed');
+    navMain.classList.remove('header__menu--opened');
+  }
+});
