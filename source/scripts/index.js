1e567f4784f5a1fc9fca78f004567d0a10468a49
Arthur <161525540+syslikmachete@users.noreply.github.com>
2026-08-28 22:58:34 +0400

    feat: complete website markup and styles (#7)
    * refactor: update index markup
    
    * chore: update slider.svg
    
    * feat: add burger menu script
    
    * feat: add slider component
    
    * feat(styles): add page block styles
    
    * styles: finish styling all components
    
    * style(common): add button styles
    
    * style(global): update global styles
    
    * style(styles): import page block
    
    * feat(html): complete catalog layout markup
    
    * feat(html): complete index layout markup
    
    * refactor: add webp format
    
    * fix: fix mentor review
    
    * refactor: add webp format and fix mentor review
    
    * fix: fix mentro review
    
    * fix: fix mentro review
    
    * refactor: add data attributes
    
    * refactor: add webp format
    
    * fix: fix mentor review
    
    * feat: add webp images
    
    * feat: add webp images
    
    * refactor: add data attributes
    
    * fix: fix minor issues
    
    * style: add color variables

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,10 @@
-/* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('[data-js-menu]');
+const navToggle = document.querySelector('[data-js-toggle]');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.dataset.state === 'closed') {
+    navMain.dataset.state = 'opened';
+  } else {
+    navMain.dataset.state = 'closed';
+  }
+});
